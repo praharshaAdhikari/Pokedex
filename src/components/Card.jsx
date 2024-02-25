7b3ce5399f1cea1b5cb5bc8ccdaafd7c435cc8d1
praharshaAdhikari <praharsha101@gmail.com>
2024-02-26 01:10:33 +0545

    Added a bit of SEO

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ const Card = ({ name, image, type }) => {
     <div class="w-72 bg-white rounded-xl duration-500 shadow-xl flex flex-col items-center">
       <img src={image} alt={name} class="h-64 w-64 object-cover rounded-t-xl" />
       <div class="px-4 py-3 w-72 bg-gray-100">
-        <p class="text-2xl font-bold text-black truncate block uppercase">
+        <h2 class="text-2xl font-bold text-black truncate block uppercase">
           {name}
-        </p>
+        </h2>
         <div class="flex items-center">
           <p class="text-lg font-semibold text-black cursor-auto my-3 capitalize">
             Type: {type}
